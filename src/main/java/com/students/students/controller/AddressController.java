43209612304f5fbc5e663d8ae3951010115c8213
adriana.ghilusi <adrianaghilusi@yahoo.com>
2022-07-11 13:10:26 +0300

    GET Request for AddressController

--- src/main/java/com/students/students/controller/AddressController.java
+++ src/main/java/com/students/students/controller/AddressController.java
@@ -1,5 +1,21 @@
 package com.students.students.controller;
 
+import com.students.students.service.AddressService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+@Controller
 public class AddressController {
+    private final AddressService addressService;
 
+    public AddressController(AddressService add) {
+        this.addressService = add;
+    }
+
+    @RequestMapping(value = "/addresses", method = RequestMethod.GET)
+    public String getAllStudents(Model model) {
+        model.addAttribute("allAddresses",addressService.getAllAddresses() );
+        return "../templates/allAddresses";
+    }
 }
--- src/main/java/com/students/students/service/AddressService.java
+++ src/main/java/com/students/students/service/AddressService.java
@@ -1,11 +1,19 @@
 package com.students.students.service;
 
+import com.students.students.model.Address;
 import com.students.students.repository.AddressRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+@Service
 public class AddressService {
 private final AddressRepository addressRepository;
 
     public AddressService(AddressRepository addressRepository) {
         this.addressRepository = addressRepository;
+    }
+
+    public List<Address> getAllAddresses() {
+        return addressRepository.findAll();
     }
 }
