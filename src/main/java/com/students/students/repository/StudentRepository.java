3c17b66f4929a8ee0cecc2290cb22f5b5e68663b
adriana.ghilusi <adrianaghilusi@yahoo.com>
2022-07-13 17:23:25 +0300

    Finished HW

--- src/main/java/com/students/students/repository/StudentRepository.java
+++ src/main/java/com/students/students/repository/StudentRepository.java
@@ -4,7 +4,10 @@ import com.students.students.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student,Integer> {
 
+    List<Student> getStudentsBySfirstnameContains(String name);
 }
